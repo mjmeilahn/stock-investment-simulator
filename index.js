7b4cbcea047d390989b569dc4184c372e26e70a6
Test <test@Tests-MacBook-Pro.local>
2024-04-04 14:15:52 -0700

    Adds local JSON databases. Basic data imports when app starts. View Account totals.

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 
 ;(async function () {
+  const fs = require('fs')
+  const axios = require('axios')
+  const fileStocks = fs.readFileSync('./invest-stocks.json', 'utf-8')
+  let stocks = JSON.parse(fileStocks)
+  const fileAccount = fs.readFileSync('./invest-account.json', 'utf-8')
+  const balance = +JSON.parse(fileAccount).total
+  const command = process.argv[2]
 
+  /*
+  GENERATE PROFIT / LOSS REPORT:
+  node index.js report
+
+  VIEW ACCOUNT:
+  node index.js account
+
+  VIEW A STOCK PRICE:
+  node index.js symbol="AAPL"
+
+  BUY A STOCK (Symbol, Shares):
+  node index.js buy="AAPL, 10"
+
+  SELL A STOCK. SELLS ALL SHARES BY DEFAULT:
+  node index.js sell="AAPL"
+  node index.js sell="AAPL, 2" (selling 2 shares of a larger amount)
+
+  SELL ALL STOCKS:
+  node index.js everythingMustGo
+  */
+
+  if (command === undefined) {
+    return console.log('\nNo arguments passed.\n')
+  }
+
+  // VIEW BANK ACCOUNT TOTALS
+  else if (command.includes('account')) {
+    console.log(`TOTAL: $${balance.toFixed(2)}`)
+  }
 })()
